c8a16e5d77412592ec00f105410d8a75199aa01b
Rafael Pelegrini <arrobarafael@hotmail.com>
2020-10-14 20:37:47 -0300

    Começado desenvolvimento do backend

---
+++ backend/src/controllers/OrphanageController.ts
@@ -0,0 +1,59 @@
+import { Request, Response } from "express";
+import { getRepository } from "typeorm";
+import Orphanage from "../models/Orphanage";
+
+export default {
+    async index(request: Request, response: Response){
+        const orphanageRepository = getRepository(Orphanage);
+        
+        const orphanages = await orphanageRepository.find();
+
+        return response.json(orphanages);
+    },
+
+    async show(request: Request, response: Response){
+        const {id} = request.params;
+        const orphanageRepository = getRepository(Orphanage);
+        
+        const orphanage = await orphanageRepository.findOneOrFail(id);
+
+        return response.json(orphanage);
+    },
+
+    async create(request: Request, response: Response){
+        const { 
+            name, 
+            latitude, 
+            longitude, 
+            about,
+            instructions,
+            opening_hours,
+            open_on_weekends  
+        } = request.body;
+    
+        const orphanageRepository = getRepository(Orphanage);
+
+        //força request.files a ser array
+        const requestImages = request.files as Express.Multer.File[];
+        const images = requestImages.map(image => {
+            return { path : image.filename }
+        })
+
+        console.log(images);
+    
+        const orphanage = orphanageRepository.create({
+            name, 
+            latitude, 
+            longitude, 
+            about,
+            instructions,
+            opening_hours,
+            open_on_weekends,
+            images
+        })
+    
+        await orphanageRepository.save(orphanage);
+    
+        return response.status(201).json(orphanage)
+    }
+}

d015ceeae0d338ec30d9477aaf019865eab652b2
Rafael Pelegrini <arrobarafael@hotmail.com>
2020-10-14 22:20:13 -0300

    Desenvolvida views para exibição dos jsons

--- backend/src/controllers/OrphanageController.ts
+++ backend/src/controllers/OrphanageController.ts
@@ -1,23 +1,32 @@
 import { Request, Response } from "express";
 import { getRepository } from "typeorm";
+import orphanageView from "../views/orphanages_view";
+
 import Orphanage from "../models/Orphanage";
+
 
 export default {
     async index(request: Request, response: Response){
         const orphanageRepository = getRepository(Orphanage);
         
-        const orphanages = await orphanageRepository.find();
+        const orphanages = await orphanageRepository.find({
+            relations: ['images']
+        });
 
-        return response.json(orphanages);
+        // return response.json(orphanages);
+        return response.json(orphanageView.renderMany(orphanages));
     },
 
     async show(request: Request, response: Response){
         const {id} = request.params;
         const orphanageRepository = getRepository(Orphanage);
         
-        const orphanage = await orphanageRepository.findOneOrFail(id);
+        const orphanage = await orphanageRepository.findOneOrFail(id, {
+            relations: ['images']
+        });
 
-        return response.json(orphanage);
+        // return response.json(orphanage);
+        return response.json(orphanageView.render(orphanage));
     },
 
     async create(request: Request, response: Response){
